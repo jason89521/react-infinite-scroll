54dbb48a0f79c19c879215f02ad9b93e51428b53
Xuan <97ssps30212@gmail.com>
2022-02-20 10:43:53 +0800

    Add tailwindcss to style demo page

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import 'demo/index.css'
+import App from 'demo/App'
 
 ReactDOM.render(
   <React.StrictMode>
